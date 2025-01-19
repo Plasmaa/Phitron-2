6d6982963ebac43bd56675bc9a1110fac2cbb921
Plasmaa <zarifpious11@gmail.com>
2025-01-19 22:52:01 +0600

    django week 3

--- 5.SDT/3.Django/Week_3_Django/Project_1/task_management/tasks/views.py
+++ 5.SDT/3.Django/Week_3_Django/Project_1/task_management/tasks/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse("This is the home page")
+def manager_dashboard(request):
+    return render(request,'dashboard/manager-dashboard.html')
 
-def contact(request):
-    return HttpResponse("<h1 style='color:red'>This is the contact page<h1/>")
+def user_dashboard(request):
+    return render(request,'dashboard/user-dashboard.html')
 
-def show_task(request):
-    return HttpResponse("<h1 style='color:blue'>This is the show_task page<h1/>")
+def test(request):
+    return render(request,'test.html')
