6847e81522697f0c675c84274a909b30249d8a7a
Plasmaa <zarifpious11@gmail.com>
2025-01-24 01:38:09 +0600

    Apply .gitignore rules and clean up untracked files

--- 5.SDT/3.Django/Week_3_Django/Project_1/task_management/tasks/forms.py
+++ 5.SDT/3.Django/Week_3_Django/Project_1/task_management/tasks/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 
 class TaskForm(forms.Form):
-    title = forms.CharField(max_length=200)
-    description = forms.CharField()
+    title = forms.CharField(max_length=200, label="Task Title")
+    description = forms.CharField(widget=forms.Textarea, label="Description")
+    due_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    # due_date = forms.DateField(widget=forms.SelectDateWidget(attrs={'type': 'date'}))
+    assigned_to = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=[])
     
-    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
